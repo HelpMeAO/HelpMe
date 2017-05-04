1115433f87536cb73facd5ce5c224a7576ff3e31
RickJdeJong <99033279@mydavinci.nl>
2017-05-04 14:33:04 +0200

    Changed a few API things
    Changed a few API things

--- api/index.js
+++ api/index.js
@@ -4,20 +4,21 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/todos', function(req, res) {
+router.get('/tickets', function(req, res) {
+  res.json({message: "LIST"});
 });
 
-router.post('/todos', function(req, res) {
+router.post('/tickets', function(req, res) {
   var todo = req.body;
 });
 
-router.put('/todos/:id', function(req, res) {
+router.put('/tickets/:id', function(req, res) {
   var id = req.params.id;
   var todo = req.body;
   res.json({ 'todo': todo, message: 'Todo Updated' });
 });
 
-router.delete('/todos/:id', function(req, res) {
+router.delete('/tickets/:id', function(req, res) {
   var id = req.params.id;
   res.json({ message: 'Todo Deleted' });
 });
