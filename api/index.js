6ebb3a211deee352b9a354d82795172a77971df6
RickJdeJong <99033279@mydavinci.nl>
2017-05-09 09:19:26 +0200

    Added tags
    Added tags

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ var express = require('express');
 
 var router = express.Router();
 
+/*** Tickets Logic ***/
+
 router.get('/tickets', function(req, res) {
   res.json({message: "LIST"});
 });
@@ -21,6 +23,27 @@ router.put('/tickets/:id', function(req, res) {
 router.delete('/tickets/:id', function(req, res) {
   var id = req.params.id;
   res.json({ message: 'Todo Deleted' });
+});
+
+/*** Tags Logic ***/
+
+router.get('/tags', function(req, res) {
+  res.json({tags: ["Node.js", "JavaScript", "HTML", "CSS"]});
+});
+
+router.post('/tags', function(req, res) {
+  res.json({message: "tag successfully added"});
+});
+
+router.put('/tags/:id', function(req, res) {
+  var id = req.params.id;
+  var todo = req.body;
+  res.json({ 'tags': tags, message: 'Todo Updated' });
+});
+
+router.delete('/tags/:id', function(req, res) {
+  var id = req.params.id;
+  res.json({ message: 'tag Deleted' });
 });
 
 module.exports = router;
