7e16c7be762970ceca448d24dc226d9704be8c91
Raymond Blok <99034225@mydavinci.nl>
2017-05-09 12:06:33 +0200

    tags page

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -6,11 +6,16 @@ var router = express.Router();
 
 router.get("/", function(req,res) {
   res.sendFile(__dirname + "/index.html");
-})
+});
 
 // Redirect to default index page
 router.get("/wachtrij", function(req,res) {
   res.sendFile(__dirname + "/Queue.html");
-})
+});
+
+// Rederict to the tags page
+router.get("/tags", function(req, res) {
+	res.sendFile(__dirname + "/tags.html");
+});
 
 module.exports = router;
