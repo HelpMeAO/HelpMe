9f1a757938deaa95dd1b1dbeb6a0f4c09cd9d52c
Raymond Blok <99034225@mydavinci.nl>
2017-05-19 11:18:44 +0200

    removed davinci auth
    removed davinci auth and all packages

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
 'use strict';
-
-module.exports = {
-    daVinciAuthValidateUrl: "http://vm:1337/Sso/ValidateAuth/0fc947b9-d986-4147-8e57-a0ca05b9d0c3/",
-    daVinciAuthKey: "NodeJSExpressAppSecret",
-    daVinciAuthNotAuthorizedRedirectUrl: "/"
-};
